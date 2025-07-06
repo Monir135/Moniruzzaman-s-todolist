55948564a4f58fb8593886a68d7679e0b6327fbb
Moniruzzaman <moniruzzaman.barbhuiya@gmail.com>
2025-07-07 02:02:53 +0530

    initial commit was done for the first version deployment

---
+++ rebuilt-todo.js
@@ -0,0 +1,46 @@
+const arrayList = [{
+    inputValue: 'make dinner',
+    dateValue: '2025-07-06'
+}, {
+    inputValue: 'wash dishes',
+    dateValue: '2025-07-07'
+}];
+let objectInsideArray;
+function displayValue() {
+    let storedValue = '';
+    for (let i = 0; i < arrayList.length; i++) {
+        objectInsideArray = arrayList[i];
+        const { inputValue, dateValue } = objectInsideArray;
+        const html = `${inputValue}</div>
+        <div> ${dateValue}</div>
+    <button onclick="
+    arrayList.splice(${i},1)
+    displayValue();
+    " class="css-button"
+    
+    >Delete</button>
+    `;
+        storedValue = storedValue + html;
+
+    }
+    const divElement = document.querySelector
+        ('.js-input-container');
+    divElement.innerHTML = storedValue;
+    console.log(storedValue)
+
+}
+displayValue();
+function todoList() {
+    const inputElement = document.querySelector('.js-todo-input');
+    let inputValue = inputElement.value;
+    const dateElement = document.querySelector('.dueDate-input');
+    let dateValue = dateElement.value;
+    arrayList.push({
+        inputValue,
+        dateValue
+    });
+    console.log(arrayList)
+    inputElement.value = '';
+    displayValue();
+
+}
